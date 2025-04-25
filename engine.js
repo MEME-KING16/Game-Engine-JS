839bd666b70dcc547b8e311e6f5093932be2650d
MEME-KING16 <115302822+MEME-KING16@users.noreply.github.com>
2025-04-24 20:02:30 -0400

    Added delta time and made start() modular

--- engine.js
+++ engine.js
@@ -7,9 +7,9 @@ class GameEngine {
         this.updatescripts = []
         this.physicsin = false
         this.physics = null
-        if(!document.getElementById(this.canvasId)) {
-            throw new Error("Canvas with the id \""+this.canvasId+"\" doen\'t exist ");
-            
+        if (!document.getElementById(this.canvasId)) {
+            throw new Error("Canvas with the id \"" + this.canvasId + "\" doen\'t exist ");
+
         }
         document.getElementById(this.canvasId).width = this.width
         document.getElementById(this.canvasId).height = this.height
@@ -23,67 +23,87 @@ class GameEngine {
     }
 
     onUpdate(script) {
-    this.updatescripts.push(script)
+        this.updatescripts.push(script)
     }
-    onKeyDown(key,script) {
+    onKeyDown(key, script) {
         addEventListener("keydown", (event) => {
-            if(event.key == key) {
-                script()
+            if (event.key == key) {
+                script(delta)
             }
         });
     }
     addPhysics(physics) {
-        if(!physics)
+        if (!physics)
             throw new Error("Physics parameter is undefined");
-            if(!(physics instanceof Physics))
+        if (!(physics instanceof Physics))
             throw new Error("Physics parameter is not a instance of Physics");
         this.physicsin = true
         this.physics = physics
     }
-start() {
-    const loop = (timestamp) => {
+    start() {
+        let last = performance.now();
+
+        const loop = (timestamp) => {
+            const delta = (timestamp - last) / 1000;
+            last = timestamp;
+
+            this._update(delta);
+            this._applyPhysics(delta);
+            this._render();
+
+            requestAnimationFrame(loop);
+        };
+
+        requestAnimationFrame(loop);
+    }
+
+    _update(delta) {
+        for (let script of this.updatescripts) {
+            script(delta); // user scripts get delta too
+        }
+    }
+
+    _applyPhysics(delta) {
+        if (!this.physicsin) return;
+
+        for (let sprite of this.sprites) {
+            sprite.speed = this.physics.calculateSpeed(delta, sprite.speed);
+            sprite.y = this.physics.calculateY(delta, sprite.speed, sprite.y);
+
+            // Bounds
+            sprite.y = Math.min(Math.max(sprite.y, 0), this.height - 12);
+            sprite.x = Math.min(Math.max(sprite.x, 0), this.width);
+        }
+    }
+
+    _render() {
         const canvas = document.getElementById(this.canvasId);
         const ctx = canvas.getContext("2d");
         ctx.clearRect(0, 0, canvas.width, canvas.height);
 
         for (let sprite of this.sprites) {
-            if (this.physicsin) {
-                sprite.speed = this.physics.calculateSpeed(0.016, sprite.speed);
-                sprite.y = this.physics.calculateY(0.016, sprite.speed, sprite.y);
-                sprite.y = Math.min(Math.max(sprite.y, 0), this.height - 12);
-                sprite.x = Math.min(Math.max(sprite.x, 0), this.width);
-            }
             ctx.drawImage(sprite.imageElement, sprite.x, sprite.y);
         }
-
-        for (let script of this.updatescripts) {
-            script();
-        }
-
-        requestAnimationFrame(loop);
-    };
-
-    requestAnimationFrame(loop);
-}
+    }
 }
 
 class Sprite {
-    constructor(img,cords) {
-        if(!img)
-        throw new Error("No sprite img given");
-        if(!cords)
-        throw new Error("No Starting cords given");
-        if(!cords.x && cords.x != 0)
-        throw new Error("No x cord given");
-        if(!cords.y && cords.y != 0)
-        throw new Error("No y cord given");
-        
+    constructor(img, cords) {
+        if (!img)
+            throw new Error("No sprite img given");
+        if (!cords)
+            throw new Error("No Starting cords given");
+        if (!cords.x && cords.x != 0)
+            throw new Error("No x cord given");
+        if (!cords.y && cords.y != 0)
+            throw new Error("No y cord given");
+
         this.img = img
         this.x = cords.x
         this.y = cords.y
         this.speed = 0
     }
-    move(x,y) {
+    move(x, y) {
         this.x = this.x + x
         this.y = this.y + y
     }
@@ -93,10 +113,10 @@ class Physics {
     constructor(opt) {
         this.gravity = opt.gravity
     }
-     calculateY(time,initSpeed,initY) {
-        return initY + initSpeed * time + this.gravity * (time*time) / 2
-     }
-     calculateSpeed(time,initSpeed) {
-        return initSpeed+this.gravity*time
-     }
+    calculateY(time, initSpeed, initY) {
+        return initY + initSpeed * time + this.gravity * (time * time) / 2
+    }
+    calculateSpeed(time, initSpeed) {
+        return initSpeed + this.gravity * time
+    }
 }
